ee1951793c4aa327bcaa302152235b9879c7e39a
Vikram Mohanty <vikram.mohanty40@gmail.com>
2023-12-15 06:10:34 +0530

    updated order page as per peer review

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -5,9 +5,14 @@ import org.openqa.selenium.By;
 public class OrderPage {
 
 	By loc = By.id("oder");
+	By price = By.id("price");
 
 	public void getOrder() {
 		System.out.println("get order");
+	}
+
+	public void getPricec() {
+		System.out.println("get price");
 	}
 
 }
